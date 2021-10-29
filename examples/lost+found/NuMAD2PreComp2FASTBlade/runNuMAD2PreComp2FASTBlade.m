70f0cfccd14db4138c7831dfc9d31e84655a7d53
Ernesto Camarena <ecamare@sandia.gov>
2021-10-29 08:29:28 -0600

    deleted lost+found/NuMAD2PreCompt2FASTBlade

--- examples/lost+found/NuMAD2PreComp2FASTBlade/runNuMAD2PreComp2FASTBlade.m
+++
@@ -1,5 +0,0 @@
-nmdfn='WindPact15MWv1.3.nmd';
-bmodes_path='C:\DesignCodes\BModes_v3.00.00\bmodes.exe';
-precomp_path='C:\DesignCodes\PreComp_v1.00.02\jcb\PreComp.exe';
-NuMAD2PreComp2FASTBlade(nmdfn,bmodes_path,precomp_path)
-
