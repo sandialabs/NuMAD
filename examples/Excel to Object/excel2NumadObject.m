1cd8790a6ff46598a1f679c7abda7c1db8db2770
Ryan Clarke <rclarke@sandia.gov>
2021-11-12 13:16:46 -0700

    Fixed issue with precomp being called from blade object

--- examples/Excel to Object/excel2NumadObject.m
+++ examples/Excel to Object/excel2NumadObject.m
@@ -8,21 +8,23 @@ addNumadPaths
 
 %% Design folder
 
-designFolder = 'C:\Users\rclarke\Desktop\Projects\BAR\NuMAD_Test\NuMAD_Excel';
-designFile = 'NREL5MW_SNL61p5.xlsx';
 
-blade = xlsBlade(fullfile(designFolder,designFile));
+designFile = 'Excel2ObjectExample.xlsx';
+
+blade = xlsBlade(designFile);
 
 
 %% update the blade object
 blade.updateBlade
 
+%% generate a blade input file using PreComp
+blade.generateBeamModel
 
 %% generate a NuMAD file
-NuMADfile = 'NREL5MW_SNL61p5.nmd';
+NuMADfile = 'Excel2ObjectExample.nmd';
 BladeDef_to_NuMADfile(blade,NuMADfile,'MatDBsi.txt');
 numad(NuMADfile)
 
 %% generate an ANSYS mesh
-blade.mesh = 0.1; % mesh size for ANSYS [m?]
-layupDesign_ANSYSmesh(blade,'NREL5MW_SNL61p5.nmd');
+% blade.mesh = 0.1; % mesh size for ANSYS [m?]
+% layupDesign_ANSYSmesh(blade,NuMADfile);
