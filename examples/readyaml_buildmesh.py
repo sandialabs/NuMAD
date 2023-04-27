3148d9e6acd7cebb5806494950248bf0dbb610bb
kbonney <kirkb1998@gmail.com>
2023-04-27 16:50:50 -0400

    Organizing examples

---
+++ examples/readyaml_buildmesh.py
@@ -0,0 +1,35 @@
+import pynumad as pynu
+import numpy as np
+import pickle
+from pprint import pprint
+
+blade = pynu.Blade()
+fileName = 'example_data/myBlade.yaml'
+blade.read_yaml(fileName)
+
+# blade.mesh = 0.2
+
+# with open('example_data/myBlade.obj','rb') as file:
+#     blade = pickle.load(file)
+
+adhes = 1
+
+meshData = blade.getShellMesh(adhes)
+
+# Print first 10 nodes coordinates
+
+pprint(meshData['nodes'][:10,:])
+
+# Print first 10 element connectivities
+
+pprint(meshData['elements'][:10,:])
+
+# Print first 10 elements in section 4, spanwise segment 10 (pressure side spar cap)
+
+# meshData['OSSets'][3,9].elementList[:10]
+
+# Print first 10 elements in spanwise segment 10 of first shear web
+
+# meshData['SWSets'][1][10].elementList[10]
+
+foo
--- examples/write_ansys_shell_model.py
+++ examples/write_ansys_shell_model.py
@@ -1,15 +1,16 @@
 import numpy as np
 from os.path import join
-
 from pynumad.analysis.ansys.ansys import writeANSYSshellModel
-from pynumad.analysis.ansys.mainAnsysAnalysis import mainAnsysAnalysis
 from pynumad.objects.Blade import Blade
 
+"""
+This example loads in a blade, gets the shell mesh, and writes the shell
+model to an ansys .src file
+"""
 
 yamlpath = join("..","data","blade_yamls","myBlade.yaml")
 blade = Blade(yamlpath)
 blade.mesh = 0.2
-
 adhes = 1
 
 meshData = blade.getShellMesh(includeAdhesive=adhes)
@@ -20,13 +21,10 @@ config["MultipleLayerBehavior"] = 'multiply'
 config["dbgen"] = 1
 config["dbname"] = 'master'
 analysisConfig = dict()
-# Set up configuration for deflection run
-analysisConfig['meshFile'] = 'master.db'
-analysisConfig['analysisFileName'] = 'bladeAnalysis'
-analysisConfig['np'] = 1
-analysisConfig['analysisFlags'] = dict()
-analysisConfig['analysisFlags']['mass'] = 0
-analysisConfig['analysisFlags']['deflection'] = 1
-defLoadsTable = []
-ansysResult = mainAnsysAnalysis(blade,meshData,defLoadsTable,analysisConfig)
 
+filename = "myblade_ansys.src"
+includeAdhesive = 1
+
+writeANSYSshellModel(blade,filename,meshData,config,includeAdhesive)
+
+whatever
